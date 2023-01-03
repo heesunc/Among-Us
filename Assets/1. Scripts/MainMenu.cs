0b37449a5ca34b40f8dcbf94f43923e7db4cb247
heesunc <heesun9113@naver.com>
2023-01-03 20:30:25 +0900

    3강
    [3강] 메뉴화면 꾸미기 : 버튼 및 파티클 만들기

---
+++ Assets/1. Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // 게임 종료 버튼 누르면 호출
+    public void ClickQuit()
+    {
+        print("게임 종료 버튼 누름");
+    }
+}
