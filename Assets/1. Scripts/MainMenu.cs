cceb636bf252d0557b6d230f9cbcca22772bbf8f
heesunc <heesun9113@naver.com>
2023-01-04 21:07:53 +0900

    4강

--- Assets/1. Scripts/MainMenu.cs
+++ Assets/1. Scripts/MainMenu.cs
@@ -7,6 +7,12 @@ public class MainMenu : MonoBehaviour
     // 게임 종료 버튼 누르면 호출
     public void ClickQuit()
     {
-        print("게임 종료 버튼 누름");
+        // 유니티 에디터
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        // 안드로이드 에디터
+#else
+        Application.Quit();
+#endif
     }
 }
